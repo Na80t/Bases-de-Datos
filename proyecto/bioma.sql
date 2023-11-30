166fd55b54936545191df485cb684ca808930dee
EduardoHdzF <90948381+EduardoHdzF@users.noreply.github.com>
2023-11-30 17:34:36 -0600

    base de datos

---
+++ proyecto/bioma.sql
@@ -0,0 +1,7 @@
+insert into bioma (tipoBioma) values ('desierto');
+insert into bioma (tipoBioma) values ('pastizales');
+insert into bioma (tipoBioma) values ('franja costera');
+insert into bioma (tipoBioma) values ('tundra');
+insert into bioma (tipoBioma) values ('aviario');
+insert into bioma (tipoBioma) values ('bosque templado');
+insert into bioma (tipoBioma) values ('bosque tropical');
